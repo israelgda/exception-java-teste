0750ab5ca9a5ac46696b5c51b2b20efeb8e016d3
Israel Gadelha <gdelha.israel@gmail.com>
2021-03-25 16:08:15 -0300

    Solução muito ruim

---
+++ src/Application/Program.java
@@ -0,0 +1,56 @@
+package Application;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+import model.entities.Reserva;
+
+public class Program {
+
+	public static void main(String[] args) throws ParseException {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		System.out.print("Número do quarto: ");
+		int num = sc.nextInt();
+		System.out.print("Data de check in: ");
+		Date ci = sdf.parse(sc.next());
+		System.out.print("Data de check out: ");
+		Date co = sdf.parse(sc.next());
+		
+		if(!co.after(ci)) {
+			System.out.println("Erro em reservar: a data de checkout é anterior a de checkin");
+		}else{
+			Reserva reserv = new Reserva(num, ci, co);
+			System.out.println("Reserva:" + reserv);
+			
+			//Atualização da reserva
+			System.out.println("Entre com a data de atualização da reserva");
+			System.out.print("Data de check in: ");
+			ci = sdf.parse(sc.next());
+			System.out.print("Data de check out: ");
+			co = sdf.parse(sc.next());
+			
+			Date now = new Date();
+			
+			if((ci.before(now)) || (co.before(now))) {
+				System.out.println("Datas de atualização devem ser em datas futuras");
+			}else if(!co.after(ci)) {
+				System.out.println("Erro em reservar: a data de checkout é anterior a de checkin");
+			}else {	
+				reserv.updateDias(ci, co);
+				System.out.println("Reserva:" + reserv);
+			}
+		}
+		
+		
+		
+		
+		
+		sc.close();
+	}
+
+}
---
+++ src/model/entities/Reserva.java
@@ -0,0 +1,52 @@
+package model.entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Reserva {
+	 private Integer numQuarto;
+	 private Date checkIn;
+	 private Date checkOut;
+	 
+	 private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	
+	 public Reserva() {
+		 
+	 }
+	 public Reserva(Integer numQuarto, Date checkIn, Date checkOut) {
+		this.numQuarto = numQuarto;
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
+	public Integer getNumQuarto() {
+		return numQuarto;
+	}
+	public void setNumQuarto(Integer numQuarto) {
+		this.numQuarto = numQuarto;
+	}
+	public Date getCheckIn() {
+		return checkIn;
+	}
+	public Date getCheckOut() {
+		return checkOut;
+	}
+	 
+	public Long duracaoDias() {
+		long diff = checkOut.getTime() - checkIn.getTime();
+		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+	}
+	
+	public void updateDias(Date checkIn, Date checkOut) {
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
+	
+	@Override
+	public String toString() {
+		return "Quarto: " + numQuarto + 
+				", check in: " + sdf.format(checkIn) +
+				", check out: " + sdf.format(checkOut) +
+				", duração: " + duracaoDias() + " noite.s";
+	}
+}
